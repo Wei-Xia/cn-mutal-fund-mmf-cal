546242ecbb100f8a3cf9faef91363d645af94ba2
Wei Xia <xw900812@gmail.com>
2024-02-20 20:48:42 +0800

    1

---
+++ rank_cal.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+
+# 假设你已经将数据读入到一个pandas DataFrame中，名为df
+df = pd.read_csv('data.csv')
+
+# 去除百分号并将列转换为浮点数类型
+df['one_year_rank'] = df['one_year_rank'].str.replace('%', '').astype(float)
+df['six_month_rank'] = df['six_month_rank'].str.replace('%', '').astype(float)
+df['three_month_rank'] = df['three_month_rank'].str.replace('%', '').astype(float)
+
+# 将 "one_year_return"、"six_month_return" 和 "three_month_return" 按照从高到低的顺序进行排名
+df['one_year_return_rank'] = df['one_year_return'].rank(ascending=False, method='min')
+df['six_month_return_rank'] = df['six_month_return'].rank(ascending=False, method='min')
+df['three_month_return_rank'] = df['three_month_return'].rank(ascending=False, method='min')
+
+# 将 "one_year_rank"、"six_month_rank" 和 "three_month_rank" 按照从低到高的顺序进行排名
+df['one_year_rank_rank'] = df['one_year_rank'].rank(ascending=True)
+df['six_month_rank_rank'] = df['six_month_rank'].rank(ascending=True)
+df['three_month_rank_rank'] = df['three_month_rank'].rank(ascending=True)
+
+# 计算排名得分
+df['rank_point'] = df['one_year_rank_rank'] + df['six_month_rank_rank'] + df['three_month_rank_rank'] + df['one_year_return_rank'] + df['six_month_return_rank'] + df['three_month_return_rank']
+
+# sharpe_ratio 从高到低的顺序进行排名
+df['sharpe_ratio_rank'] = df['sharpe_ratio'].rank(ascending=False, method='min')
+
+# 设置权重
+weights = {'收益得分': 0.9, '风险得分': 0.1}
+
+# 计算综合得分
+df['综合得分'] = df['rank_point'] * weights['收益得分'] + df['sharpe_ratio_rank'] * weights['风险得分']
+
+# 根据综合得分进行排名
+df['排名'] = df['综合得分'].rank(ascending=False)
+
+# 输出排名结果
+# print(df[['代码', '名称 (A)', '综合得分']].to_string(index=False))
+print(df[['综合得分']].to_string(index=False))
+
