80182c3af0b3e0fb657708cac8df0fa473751b85
Wei Xia <xw900812@gmail.com>
2024-02-20 21:39:21 +0800

    1
    1

--- rank_cal.py
+++ rank_cal.py
@@ -10,9 +10,9 @@ df['six_month_rank'] = df['six_month_rank'].str.replace('%', '').astype(float)
 df['three_month_rank'] = df['three_month_rank'].str.replace('%', '').astype(float)
 
 # 将 "one_year_return"、"six_month_return" 和 "three_month_return" 按照从高到低的顺序进行排名
-df['one_year_return_rank'] = df['one_year_return'].rank(ascending=False, method='min')
-df['six_month_return_rank'] = df['six_month_return'].rank(ascending=False, method='min')
-df['three_month_return_rank'] = df['three_month_return'].rank(ascending=False, method='min')
+df['one_year_return_rank'] = df['one_year_return'].rank(ascending=False, method='first')
+df['six_month_return_rank'] = df['six_month_return'].rank(ascending=False, method='first')
+df['three_month_return_rank'] = df['three_month_return'].rank(ascending=False, method='first')
 
 # 将 "one_year_rank"、"six_month_rank" 和 "three_month_rank" 按照从低到高的顺序进行排名
 df['one_year_rank_rank'] = df['one_year_rank'].rank(ascending=True)
@@ -23,7 +23,7 @@ df['three_month_rank_rank'] = df['three_month_rank'].rank(ascending=True)
 df['rank_point'] = df['one_year_rank_rank'] + df['six_month_rank_rank'] + df['three_month_rank_rank'] + df['one_year_return_rank'] + df['six_month_return_rank'] + df['three_month_return_rank']
 
 # sharpe_ratio 从高到低的顺序进行排名
-df['sharpe_ratio_rank'] = df['sharpe_ratio'].rank(ascending=False, method='min')
+df['sharpe_ratio_rank'] = df['sharpe_ratio'].rank(ascending=False, method='first')
 
 # 设置权重
 weights = {'收益得分': 0.9, '风险得分': 0.1}
@@ -35,6 +35,6 @@ df['综合得分'] = df['rank_point'] * weights['收益得分'] + df['sharpe_rat
 df['排名'] = df['综合得分'].rank(ascending=False)
 
 # 输出排名结果
-print(df[['Code', 'Name', '综合得分']].to_string(index=False))
-# print(df[['综合得分']].to_string(index=False))
+# print(df[['Code', 'Name', '综合得分']].to_string(index=False))
+print(df[['综合得分']].to_string(index=False))
 
