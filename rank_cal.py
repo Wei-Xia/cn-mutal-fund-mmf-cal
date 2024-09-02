6fb5ffec8489850905f6f0bd15977a26a5afb7f1
Wei Xia <xw900812@gmail.com>
2024-09-02 17:24:27 +0800

    111

--- rank_cal.py
+++ rank_cal.py
@@ -23,7 +23,7 @@ df['three_month_rank_rank'] = df['three_month_rank'].rank(ascending=True)
 df['rank_point'] = df['one_year_rank_rank'] + df['six_month_rank_rank'] + df['three_month_rank_rank'] + df['one_year_return_rank'] + df['six_month_return_rank'] + df['three_month_return_rank']
 
 # sharpe_ratio 从高到低的顺序进行排名
-df['sharpe_ratio_rank'] = df['sharpe_ratio'].rank(ascending=False, method='first')
+df['sharpe_ratio_rank'] = df['sharpe_ratio'].rank(ascending=True, method='first')
 
 # 设置权重
 weights = {'收益得分': 0.9, '风险得分': 0.1}
@@ -38,3 +38,4 @@ df['排名'] = df['综合得分'].rank(ascending=False)
 print(df[['Code', 'Name', '综合得分']].to_string(index=False))
 # print(df[['综合得分']].to_string(index=False))
 
+# print(df[['Name','sharpe_ratio_rank']].to_string(index=False))
