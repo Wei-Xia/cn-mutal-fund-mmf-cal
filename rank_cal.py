7db7fe076a55e14505436c0450574d4a93d27b16
Wei Xia <xw900812@gmail.com>
2025-01-01 21:35:06 +0800

    111

--- rank_cal.py
+++ rank_cal.py
@@ -1,43 +1,26 @@
 import pandas as pd
-import numpy as np
 
-# 假设你已经将数据读入到一个pandas DataFrame中，名为df
-df = pd.read_csv('data.csv')
+# 读取 CSV 文件
+file_path = 'data.csv'  # 替换为你的 CSV 文件路径
+data = pd.read_csv(file_path)
 
-# 去除百分号并将列转换为浮点数类型
-df['one_year_rank'] = df['one_year_rank'].str.replace('%', '').astype(float)
-df['six_month_rank'] = df['six_month_rank'].str.replace('%', '').astype(float)
-df['three_month_rank'] = df['three_month_rank'].str.replace('%', '').astype(float)
+# 定义需要排名的列
+ranking_columns = ['one_month_return', 'three_month_return', 'six_month_return', 'one_year_return']
 
-# 将 "one_year_return"、"six_month_return" 和 "three_month_return" 按照从高到低的顺序进行排名
-df['one_year_return_rank'] = df['one_year_return'].rank(ascending=False, method='first')
-df['six_month_return_rank'] = df['six_month_return'].rank(ascending=False, method='first')
-df['three_month_return_rank'] = df['three_month_return'].rank(ascending=False, method='first')
+# 为每列添加排名
+for col in ranking_columns:
+    rank_col = f'{col}_rank'
+    data[rank_col] = data[col].rank(ascending=False, method='min')
 
-# 将 "one_year_rank"、"six_month_rank" 和 "three_month_rank" 按照从低到高的顺序进行排名
-df['one_year_rank_rank'] = df['one_year_rank'].rank(ascending=True)
-df['six_month_rank_rank'] = df['six_month_rank'].rank(ascending=True)
-df['three_month_rank_rank'] = df['three_month_rank'].rank(ascending=True)
+# 使用 apply 计算总和
+data['total_rank_number'] = data[[f'{col}_rank' for col in ranking_columns]].apply(lambda row: row.sum(), axis=1)
 
-# 计算排名得分
-df['rank_point'] = df['one_year_rank_rank'] + df['six_month_rank_rank'] + df['three_month_rank_rank'] + df['one_year_return_rank'] + df['six_month_return_rank'] + df['three_month_return_rank']
+# 将 total_rank_number 转换为整数
+data['total_rank_number'] = data['total_rank_number'].astype(int)
 
-# sharpe_ratio 从高到低的顺序进行排名
-# df['sharpe_ratio_rank'] = df['sharpe_ratio'].rank(ascending=True, method='first')
+# 打印 Fund Name, Fund Code 和 total_rank_number
+# print(data[['Fund Name', 'Fund Code', 'total_rank_number']])
 
-# 设置权重
-weights = {'收益得分': 1, '风险得分': 0.1}
-
-# 计算综合得分
-# df['综合得分'] = df['rank_point'] * weights['收益得分'] + df['sharpe_ratio_rank'] * weights['风险得分']
-df['综合得分'] = df['rank_point'] * weights['收益得分'] 
-
-
-# 根据综合得分进行排名
-df['排名'] = df['综合得分'].rank(ascending=False)
-
-# 输出排名结果
-# print(df[['Code', 'Name', '综合得分']].to_string(index=False))
-print(df[['综合得分']].to_string(index=False))
-
-# print(df[['Name','sharpe_ratio_rank']].to_string(index=False))
+# 打印 total_rank_number 列，仅显示数字
+for value in data['total_rank_number']:
+    print(value)
